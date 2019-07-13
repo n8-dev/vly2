1c2cbd17aa04fbd1ac3386527514dbc67e040b87
Sam Walsh <Sam.Walsh@serko.com>
2019-07-14 11:58:09 +1200

    Tidied up date filter styling

--- components/Search/Filters/DateFilter.js
+++ components/Search/Filters/DateFilter.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { FormattedMessage, FormattedDate } from "react-intl";
+import { FormattedMessage } from "react-intl";
 import styled from "styled-components";
 import { FilterItem } from "../BigSearch";
-import { Popover, Button, Dropdown, Menu, Select } from "antd";
+import { Popover, Button, Select, Form as AntdForm } from "antd";
 import DatePickerComponent, {
   formatDateBaseOn
 } from "../../../pages/search/DatePickerComponent";
@@ -11,16 +11,22 @@ import DatePickerType from "../../Op/DatePickerType.constant";
 
 const Option = Select.Option;
 
-const Form = styled.div``;
-
 const Footer = styled.div`
   display: flex;
   justify-content: flex-end;
+  margin-top: 1rem;
+`;
+
+const Form = styled(AntdForm)`
+  > *:last-child {
+    margin-right: 0;
+  }
 `;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
+  max-width: 1000vw;
 `;
 
 class DateFilter extends Component {
@@ -38,11 +44,11 @@ class DateFilter extends Component {
     this.state.popoverValue = this.state.value;
   }
 
-  handleChangeDatePickerType = (datePickerType) => {
+  handleChangeDatePickerType = datePickerType => {
     this.setState({
-      popoverDatePickerType: datePickerType 
-    })
-  }
+      popoverDatePickerType: datePickerType
+    });
+  };
 
   changePickerTypeIndividualDate = () => {
     this.setState({ popoverDatePickerType: DatePickerType.IndividualDate });
@@ -110,66 +116,66 @@ class DateFilter extends Component {
 
     const content = (
       <Content>
-        <Form>
-          <Select
-            placeholder={
-              <FormattedMessage
-                id="search.filter.date.picker.option.date.label"
-                defaultMessage="Date"
-              />
-            }
-            onChange={this.handleChangeDatePickerType}
-            defaultValue={datePickerType}
-            style={{ width: 200 }}
-          >
-            <Option
-              key="by-date"
-              value={DatePickerType.IndividualDate}
+        <Form layout="inline" onSubmit={this.handleSubmit}>
+          <Form.Item>
+            <Select
+              placeholder={
+                <FormattedMessage
+                  id="search.filter.date.picker.option.date.label"
+                  defaultMessage="Date"
+                />
+              }
+              onChange={this.handleChangeDatePickerType}
+              defaultValue={datePickerType}
+              style={{ width: 200 }}
             >
-              <FormattedMessage
-                id="search.filter.date.option.date.label"
-                defaultMessage="Specific date"
-              />
-            </Option>
-            <Option
-              key="by-week"
-              value={DatePickerType.WeekRange}
-            >
-              <FormattedMessage
-                id="search.filter.date.option.week.label"
-                defaultMessage="Week"
-              />
-            </Option>
-            <Option
-              key="by-month"
-              value={DatePickerType.MonthRange}
-            >
-              <FormattedMessage
-                id="search.filter.date.option.month.label"
-                defaultMessage="Month"
-              />
-            </Option>
-            <Option
-              key="by-date-range"
-              value={DatePickerType.DateRange}
-            >
-              <FormattedMessage
-                id="search.filter.date.option.dateRange.label"
-                defaultMessage="Date-range"
-              />
-            </Option>
-          </Select>
-          <DatePickerComponent
-            datePickerType={popoverDatePickerType}
-            onDateChange={this.handleChange}
-            dateValue={popoverValue}
-          />
+              <Option key="by-date" value={DatePickerType.IndividualDate}>
+                <FormattedMessage
+                  id="search.filter.date.option.date.label"
+                  defaultMessage="Specific date"
+                />
+              </Option>
+              <Option key="by-week" value={DatePickerType.WeekRange}>
+                <FormattedMessage
+                  id="search.filter.date.option.week.label"
+                  defaultMessage="Week"
+                />
+              </Option>
+              <Option key="by-month" value={DatePickerType.MonthRange}>
+                <FormattedMessage
+                  id="search.filter.date.option.month.label"
+                  defaultMessage="Month"
+                />
+              </Option>
+              <Option key="by-date-range" value={DatePickerType.DateRange}>
+                <FormattedMessage
+                  id="search.filter.date.option.dateRange.label"
+                  defaultMessage="Date-range"
+                />
+              </Option>
+            </Select>
+          </Form.Item>
+          <Form.Item>
+            <DatePickerComponent
+              datePickerType={popoverDatePickerType}
+              onDateChange={this.handleChange}
+              dateValue={popoverValue}
+            />
+          </Form.Item>
         </Form>
         <Footer>
-          <Button onClick={this.handleCancel}>Cancel</Button>
-          <Button onClick={this.handleSubmit} type="primary">
-            Submit
-          </Button>
+          <Form layout="inline" onSubmit={this.handleSubmit}>
+          <Form.Item>
+            <Button onClick={this.handleCancel}>
+              <FormattedMessage id="search.filter.popover.cancel.label" defaultMessage="Cancel" />
+            </Button>
+          </Form.Item>
+          <Form.Item>
+            <Button onClick={this.handleSubmit} type="primary">
+              <FormattedMessage id="search.filter.popover.submit.label" defaultMessage="Apply filter" />
+            </Button>
+          </Form.Item>
+          </Form>
         </Footer>
       </Content>
     );
